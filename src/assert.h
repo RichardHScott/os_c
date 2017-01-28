877860d63a958940e9e7182ad462410f2cca1d90
Richard Scott <richard.scott@vee24.com>
2017-01-28 16:47:52 +0000

    Add definition for os assert to assert.h.

--- src/assert.h
+++ src/assert.h
@@ -5,5 +5,6 @@
 #else
 #define STRINGIFY(x) #x
 #define TOSTRING(x) STRINGIFY(x)
+int _os_assert(const char*, const char*, const char*);
 #define assert(EX) (void)( (EX) || _os_assert(#EX, __FILE__, TOSTRING(__LINE__)) )
 #endif
